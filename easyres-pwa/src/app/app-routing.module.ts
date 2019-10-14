0288a13b265c84e5e94853b8edaf8371d13d2273
Davy <davy.cools@student.ap.be>
2019-10-14 17:05:43 +0200

    Toevoegen van routing voor restaurant info
    + Restaurant info wordt nu weergegeven  in de routing als volgt:
    - restaurant/restaurantId (restaurant/1)

--- easyres-pwa/src/app/app-routing.module.ts
+++ easyres-pwa/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
+import { RestaurantInfoComponent } from './restaurant-info/restaurant-info.component';
 
 
 const routes: Routes = [
   {path:"restaurant", component:RestaurantComponent},
+  {path: 'restaurant/:restaurant.restaurantId', component: RestaurantInfoComponent },
   {path:"", redirectTo:"restaurant", pathMatch:"full"},
   {path:"**", redirectTo:"home", pathMatch:"full"}
 ];
