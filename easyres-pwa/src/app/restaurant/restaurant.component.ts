df7eb8b2e0e4d47054c7e6f8e6a1867960e28046
DavyCools <DavyCools@users.noreply.github.com>
2019-11-08 21:09:23 +0100

    Mogelijkheid tot toevoegen van restaurants aan favorieten
    + Get's async gemaakt voor
    + mogelijkheid tot toevoegen van restaurants aan favorieten

--- easyres-pwa/src/app/restaurant/restaurant.component.ts
+++ easyres-pwa/src/app/restaurant/restaurant.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RestaurantService, IRestaurant } from '../services/restaurant.service';
+import { MsalService } from '../services/msal.service';
 
 
 @Component({
@@ -12,15 +13,17 @@ export class RestaurantComponent implements OnInit {
   Restaurants : IRestaurant[];
   sorterenOp: string = "Aanbevolen";
   
-  constructor(private ResService : RestaurantService) { }
+  constructor(private ResService : RestaurantService, private msalService: MsalService) { }
 
   zoeknaam: string;
   zoekterm: string;
   sorteerKeuzes: string[] = ["Aanbevolen","Naam","Type","Soort","Gemeente","Land"];
   types: type[] = [{naam: "Restaurant",active:true},{naam: "Taverne",active:true},{naam: "Bistro",active:true},{naam: "Trattoria",active:true}]
+  UserId: string;
 
   async ngOnInit() {
-    this.GetRestaurants();
+    this.GetUserObjectId();
+    await this.GetRestaurants();
   }
   Zoeken(){
     this.zoekterm = `naam=${this.zoeknaam}`;
@@ -34,22 +37,53 @@ export class RestaurantComponent implements OnInit {
     else
       this.GetRestaurants();
   }
-  GetRestaurants(){
+  async GetRestaurants(){
     var temp: IRestaurant[] = [];
-    this.types.forEach(element => {
-      if(element.active){
-        this.ResService.GetRestaurants(`${this.zoekterm}&soort=${element.naam}`).subscribe(restaurants => {
-          restaurants.forEach(element => {
-            temp.push(element);
-          });
-        })
+    for(var element of this.types){
+      if (element.active) {
+        var tempRestaurants = await this.ResService.GetRestaurants(`${this.zoekterm}&soort=${element.naam}`);
+        tempRestaurants.forEach(element => {
+          temp.push(element);
+        });
       }
-    });
+    }
     this.Restaurants = temp;
+    await this.CheckFavorites();
   }
   ChangeTypes(type){
     type.active = !type.active;
     this.GetRestaurants();
+  }
+  isUserLoggedIn() {
+    return this.msalService.isLoggedIn();
+  }
+  GetUserObjectId(){
+    this.UserId = this.msalService.getUserObjectId();
+  }
+  AddDeleteFavorites(Restaurantid: number, index){
+    if(this.RestaurantsFavoriteBooleans[index]){
+      this.ResService.DeleteFavoritesByID(this.UserId, Restaurantid).subscribe();
+    }
+    else{
+      this.ResService.PostFavorite(this.UserId,Restaurantid).subscribe();
+    }
+    this.RestaurantsFavoriteBooleans[index] = !this.RestaurantsFavoriteBooleans[index];
+  }
+  FavoriteRestaurants: IRestaurant[] = []
+  async GetUserFavorites(){
+    var tempGebruiker = await this.ResService.GetFavorites(this.UserId,"");
+    this.FavoriteRestaurants = tempGebruiker.restaurants;
+  }
+  RestaurantsFavoriteBooleans: boolean[] = [];
+  async CheckFavorites(){
+    await this.GetUserFavorites();
+    this.RestaurantsFavoriteBooleans = [];
+    this.Restaurants.forEach(element => {
+      if(this.FavoriteRestaurants.find(x => x.restaurantId === element.restaurantId))
+        this.RestaurantsFavoriteBooleans.push(true);
+      else
+      this.RestaurantsFavoriteBooleans.push(false);
+    });
   }
 }
 export interface type{
