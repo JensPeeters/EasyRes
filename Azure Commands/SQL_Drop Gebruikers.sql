4e2255f2d98e3541a446b5758dc6e89846866619
Jens Peeters <jenspeeters99@hotmail.com>
2019-11-12 11:50:35 +0100

    Update SQL Drop

--- Azure Commands/SQL_Drop Gebruikers.sql
+++ Azure Commands/SQL_Drop Gebruikers.sql
@@ -1,2 +1,3 @@
-ALTER TABLE [dbo].[Restaurants] DROP CONSTRAINT [FK_Restaurants_Gebruikers_GebruikersID];
+-- ALTER TABLE [dbo].[Sessies] DROP CONSTRAINT [FK_Sessies_Gebruikers_GebruikersID];
+-- ALTER TABLE [dbo].[Restaurants] DROP CONSTRAINT [FK_Restaurants_Gebruikers_GebruikersID];
 DROP TABLE [dbo].[Gebruikers];
