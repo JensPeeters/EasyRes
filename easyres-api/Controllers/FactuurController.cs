3c27242313e38c3f6aabdb3922a83ebc617a7f13
Jens Peeters <jenspeeters99@hotmail.com>
2019-11-27 13:08:39 +0100

    bugfix test

--- easyres-api/Controllers/FactuurController.cs
+++ easyres-api/Controllers/FactuurController.cs
@@ -131,12 +131,12 @@ namespace easyres_api.Controllers
             {
                 //Normaal stuur je de renderer niet mee
                 string msg = "In bijlage vindt u de factuur van u bezoek aan " + factuur.Restaurant.Naam + ".";
-                //emailSender.SendEmailAsync(mail,
-                //                           "Factuur van " + factuur.Restaurant.Naam,
-                //                           msg).Wait();
                 emailSender.SendEmailAsync(mail,
-                "Factuur van " + factuur.Restaurant.Naam,
-                                           msg, factuur.Id, pdfGenerator.GetStream()).Wait();
+                                           "Factuur van " + factuur.Restaurant.Naam,
+                                           msg).Wait();
+                //emailSender.SendEmailAsync(mail,
+                //"Factuur van " + factuur.Restaurant.Naam,
+                //                           msg,factuur.Id,pdfGenerator.GetStream()).Wait();
             }
             return Created("", factuur);
         }
