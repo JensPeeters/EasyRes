ef7ff7fe44dc152d4d11894e1fed013743bb3931
Jens Peeters <jenspeeters99@hotmail.com>
2019-12-02 23:39:20 +0100

    Sorteren toevoegen aan Front-end
    * Toevoegen van sorteren aan Reservaties
    *  Toevoegen van sorteren aan Facturen

--- easyres-pwa/src/app/facturen/facturen.component.ts
+++ easyres-pwa/src/app/facturen/facturen.component.ts
@@ -12,15 +12,28 @@ export class FacturenComponent implements OnInit {
 
   Facturen: IFactuur[] = [];
   UserId: string;
+
+  SorteerOp = 'Datum';
+  sorteerKeuzes: string[] = ['Datum', 'Restaurant', 'Factuurnummer', 'Prijs'];
+
   constructor(private factuurService: FactuurService, private msalService: MsalService) { }
 
   ngOnInit() {
     if (this.msalService.isLoggedIn()) {
       this.GetUserObjectId();
     }
-    this.factuurService.GetFacturen(this.UserId).subscribe( res => {
+    this.getFacturen();
+  }
+
+  getFacturen() {
+    this.factuurService.GetFacturen(this.UserId, this.SorteerOp).subscribe( res => {
       this.Facturen = res;
     });
+  }
+
+  Sorteren(item) {
+    this.SorteerOp = item;
+    this.getFacturen();
   }
 
   GetUserObjectId() {
