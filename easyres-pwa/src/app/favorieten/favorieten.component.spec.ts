1962e8fc9388ce736b33d6e20eab0470fa623776
Davy <davy.cools@student.ap.be>
2019-12-02 23:15:38 +0100

    Toevoegen van enkele basistesten

--- easyres-pwa/src/app/favorieten/favorieten.component.spec.ts
+++ easyres-pwa/src/app/favorieten/favorieten.component.spec.ts
@@ -27,4 +27,24 @@ describe('FavorietenComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Expect IsEmpty() to return true when Restaurants is empty', () => {
+    var Restaurants = [];
+    var result = component.IsEmpty(Restaurants);
+    expect(result).toBe(true);
+  });
+
+  it('Zoeknaam should be equel to the text in the searchinput and zoekterm should be updated to (naam={zoeknaam}) when clicked on searchicon', () => {
+    fixture.detectChanges();
+    const htmlElement : HTMLElement = fixture.nativeElement;
+    let input = htmlElement.querySelector('input');
+    let span = htmlElement.querySelector('span');
+    input.value = 'restaurantnaam';
+    input.dispatchEvent(new Event('input'));
+    span.click();
+    fixture.detectChanges();
+    expect(component.zoeknaam).toBe("restaurantnaam");
+    expect(component.zoekterm).toBe("naam=restaurantnaam");
+  });
+
 });
