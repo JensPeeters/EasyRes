0462e051c3ada6ebe4d82ffb9c5ea81d05e6c043
Hector <hector.brasfalean@student.ap.be>
2019-12-02 19:40:04 +0100

    Toevoegen gitignores

--- easyres-api/easyres-api/Startup.cs
+++ easyres-api/easyres-api/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Business_layer.Facades;
 using Business_layer.Interfaces;
+using Data_layer.Interfaces;
 using Data_layer.Model;
+using Data_layer.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -29,13 +31,19 @@ namespace easyres_api
                 )
             );
             services.AddTransient<ISessieFacade, SessieFacade>();
-            services.AddTransient<ISessieFacade, SessieFacade>();
-            services.AddTransient<ISessieFacade, SessieFacade>();
-            services.AddTransient<ISessieFacade, SessieFacade>();
-            services.AddTransient<ISessieFacade, SessieFacade>();
-            services.AddTransient<ISessieFacade, SessieFacade>();
-            services.AddTransient<ISessieFacade, SessieFacade>();
-            services.AddTransient<ISessieFacade, SessieFacade>();
+            services.AddTransient<IFactuurFacade, FactuurFacade>();
+            services.AddTransient<IRestaurantFacade, RestaurantFacade>();
+            services.AddTransient<IReserveringenFacade, ReserveringenFacade>();
+            services.AddTransient<IFavorietenFacade, FavorietenFacade>();
+            services.AddTransient<IBestellingenFacade, BestellingenFacade>();
+            services.AddTransient<IUserFacade, UserFacade>();
+            services.AddTransient<ISessieRepository, SessieRepository>();
+            services.AddTransient<IFactuurRepository, FactuurRepository>();
+            services.AddTransient<IRestaurantRepository, RestaurantRepository>();
+            services.AddTransient<IReserveringenRepository, ReserveringenRepository>();
+            services.AddTransient<IFavorietenRepository, FavorietenRepository>();
+            services.AddTransient<IBestellingenRepository, BestellingenRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
