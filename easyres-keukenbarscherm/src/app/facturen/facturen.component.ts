fe12c51f1c77c1f947654e53822550882867dc3c
Hector <hector.brasfalean@student.ap.be>
2019-12-07 12:49:43 +0100

    Weergeven betaalde/openstaande facturen voor uitbater

---
+++ easyres-keukenbarscherm/src/app/facturen/facturen.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService, IUitbater, IFactuur } from '../services/user.service';
+import { MsalService } from '../services/msal.service';
+
+@Component({
+  selector: 'app-facturen',
+  templateUrl: './facturen.component.html',
+  styleUrls: ['./facturen.component.scss']
+})
+export class FacturenComponent implements OnInit {
+  BetaaldeFacturenView : boolean = true;
+  BetaaldeFacturen : IFactuur[] = [];
+  OpenstaandeFacturen : IFactuur[] = [];
+  uitbater: IUitbater;
+  constructor(private userService : UserService,
+    private MsalService : MsalService) { }
+
+  ngOnInit() {
+    this.userService.isuitbater(this.MsalService.getUserObjectId()).subscribe(res =>{
+      this.uitbater = res;
+      this.userService.GetFacturenUitbater(this.uitbater.restaurantId).subscribe(res => {
+        res.map(factuur => {
+          if(factuur.betaald)
+            this.BetaaldeFacturen.push(factuur);
+          else if(!factuur.betaald)
+            this.OpenstaandeFacturen.push(factuur);
+        });
+        console.log(this.BetaaldeFacturen);
+        console.log(this.OpenstaandeFacturen);
+      });
+    });
+  }
+
+  ChangeRequest(bool : boolean){
+    this.BetaaldeFacturenView = bool;
+  }
+}
--- easyres-keukenbarscherm/src/app/services/user.service.ts
+++ easyres-keukenbarscherm/src/app/services/user.service.ts
@@ -18,9 +18,54 @@ export class UserService {
   isuitbater(UserId) {
     return this.http.get<IUitbater>(`${this.urlAPI}/user/isuitbater/${UserId}`);
   }
+
+  GetFacturenUitbater(resId : number){
+    return this.http.get<IFactuur>(`${this.urlAPI}/factuur/restaurant/${resId}`);
+  }
 }
 
 export interface IUitbater {
   gebruikersID: string;
   restaurantId: number;
+}
+
+export interface IFactuur {
+  id: number;
+  producten: IProduct[];
+  gebruiker: IGebruiker;
+  restaurant: IRestaurant;
+  betaald: boolean;
+  totaalPrijs: number;
+  tafelNr: number;
+  datum: Date;
+}
+export interface ITafel {
+  tafelId: number;
+  tafelnr: number;
+  zitplaatsen: number;
+}
+export interface IRestaurant {
+  restaurantId: number;
+  naam: string;
+  type: string;
+  soort: string;
+  locatie: any;
+  menu: any;
+  tafels: ITafel[];
+  openingsuren: any;
+  gerechten: string;
+  korteBeschrijving: string;
+  langeBeschrijving: string;
+  logoImage: string;
+  isAdvertentie: boolean;
+}
+export interface IGebruiker {
+  gebruikersID: string;
+  favorieten: IRestaurant[];
+  getFactuurByEmail: boolean;
+}
+export interface IProduct {
+  naam: string;
+  prijs: number;
+  aantal: number;
 }
